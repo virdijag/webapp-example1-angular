9a52adc650bab96e6321f026882a5932cbc5a944
Jagdeep Virdi <virdijag@hotmail.com>
2020-04-23 14:36:26 +0100

    added faTimes icon to articles

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html'
 })
 export class ListComponent {
-
+  faTimes = faTimes;
   @Input() articleList;
 }
